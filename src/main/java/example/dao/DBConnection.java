b1c5f3dcac1502473d07d0b8753a9a062629ef71
Vo Duong Thien <conik2203@gmail.com>
2025-12-03 21:33:41 +0700

    update_from_eclipe

--- src/main/java/example/dao/DBConnection.java
+++ src/main/java/example/dao/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
 	private static final String URL = "jdbc:mysql://localhost:3306/movie?useSSL=false&serverTimezone=UTC&characterEncoding=UTF-8";
 	private static final String USER = "root";
-	private static final String PASSWORD = "123456";
+	private static final String PASSWORD = "123456";// moi ne
 
 	public static Connection getConnection() {
 		Connection c = null;
