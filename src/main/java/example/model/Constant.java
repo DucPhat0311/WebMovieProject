ae74014d8c69726a68921a0485063d0d6d73a42d
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-05 16:12:22 +0700

    them constant class trong model

---
+++ src/main/java/example/model/Constant.java
@@ -0,0 +1,22 @@
+package example.model;
+
+public class Constant {
+	public static final String BOOKING_PENDING = "Pending";
+    public static final String BOOKING_SUCCESS = "Success";
+    public static final String BOOKING_CANCELLED = "Cancelled";
+    
+    public static final String PAYMENT_PENDING = "Pending";
+    public static final String PAYMENT_SUCCESS = "Success";
+    public static final String PAYMENT_FAILED = "Failed";
+    
+    public static final String METHOD_MOMO = "Momo";
+    public static final String METHOD_VNPAY = "VNPay";
+    public static final String METHOD_BANK = "BankPro";
+    
+    public static final String ROLE_ADMIN = "admin";
+    public static final String ROLE_USER = "user";
+    
+    public static final String MOVIE_COMING_SOON = "Coming Soon";
+    public static final String MOVIE_NOW_SHOWING = "Now Showing";
+    public static final String MOVIE_ENDED = "Ended";
+}
