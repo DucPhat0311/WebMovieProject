85a3040b8b0955f01ca7dcafbb6a167adcc0dc7d
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-05 21:13:03 +0700

    tao phuong thuc  tim kiem date cho 1 movie

--- src/main/java/example/dao/ShowtimeDAO.java
+++ src/main/java/example/dao/ShowtimeDAO.java
@@ -1,5 +1,28 @@
 package example.dao;
 
-public class ShowtimeDAO {
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ShowtimeDAO {
+	public List<Date> getShowDates(int movieId) {
+        List<Date> dates = new ArrayList<>();
+        String sql = "SELECT DISTINCT show_date FROM Showtime "
+                   + "WHERE movie_id = ? AND show_date >= CURDATE() AND is_active = TRUE "
+                   + "ORDER BY show_date ASC";
+        
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            
+            ps.setInt(1, movieId);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                dates.add(rs.getDate("show_date"));
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+        return dates;
+    }
 }
