ca5cba90eba8b88c6fac672271d804d15547d2f2
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-05 15:38:35 +0700

    deleted strategy package

--- src/main/java/example/strategy/Payment.java
+++
@@ -1,5 +0,0 @@
-package example.strategy;
-
-public interface Payment {
-	void pay(double amount);
-}
--- src/main/java/example/strategy/ViettelMoney.java
+++
@@ -1,9 +0,0 @@
-package example.strategy;
-
-public class ViettelMoney implements Payment {
-
-	@Override
-	public void pay(double amount) {
-	    System.out.println("You have just made a payment with ViettelMoney of " + amount + " VND. Good job!!!");
-	}
-}
