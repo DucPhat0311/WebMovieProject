a9308883f409feec5d241b713c97032074bccb31
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-05 16:43:04 +0700

    sua thuoc tinh cho 4 file bookingdetail cinema room seattype

--- src/main/java/example/model/BookingDetail.java
+++ src/main/java/example/model/BookingDetail.java
@@ -1,5 +1,52 @@
 package example.model;
 
 public class BookingDetail {
+	private int bookingDetailId;
+    private int bookingId;
+    private int seatId;
+    private double price;
+    
+	public BookingDetail(int bookingDetailId, int bookingId, int seatId, double price) {
+		super();
+		this.bookingDetailId = bookingDetailId;
+		this.bookingId = bookingId;
+		this.seatId = seatId;
+		this.price = price;
+	}
 
+	public int getBookingDetailId() {
+		return bookingDetailId;
+	}
+
+	public void setBookingDetailId(int bookingDetailId) {
+		this.bookingDetailId = bookingDetailId;
+	}
+
+	public int getBookingId() {
+		return bookingId;
+	}
+
+	public void setBookingId(int bookingId) {
+		this.bookingId = bookingId;
+	}
+
+	public int getSeatId() {
+		return seatId;
+	}
+
+	public void setSeatId(int seatId) {
+		this.seatId = seatId;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	} 
+	
+	
+
+    
 }
