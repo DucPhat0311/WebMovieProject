bb22925657160ad3e2d22f9e4698604e1282afa0
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-06 12:57:32 +0700

    them thuoc tinh, constructor, getter setter cho artist va genre class

--- src/main/java/example/model/Genre.java
+++ src/main/java/example/model/Genre.java
@@ -1,5 +1,36 @@
 package example.model;
 
 public class Genre {
+    private int genreId;
+    private String name;
 
+    public Genre() {
+    }
+
+    public Genre(int genreId, String name) {
+        this.genreId = genreId;
+        this.name = name;
+    }
+
+    // Getters & Setters
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(int genreId) {
+        this.genreId = genreId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
