fd78347e303bb56853862d140519d19d2298e875
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-23 17:11:53 +0700

    them servlet cho chuc nang load more

--- src/main/java/example/controller/LoadMoreServlet.java
+++ src/main/java/example/controller/LoadMoreServlet.java
@@ -15,7 +15,7 @@ import example.model.Movie;
 /**
  * Servlet implementation class LoadMoreServlet
  */
-@WebServlet("/load-more-movies")
+@WebServlet("/load-more")
 public class LoadMoreServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -31,29 +31,22 @@ public class LoadMoreServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		response.setContentType("text/html;charset=UTF-8");
-
-		// 1. Lấy số lượng phim ĐANG hiển thị trên màn hình (Client gửi lên)
-		String amountExistStr = request.getParameter("existed");
-		int amountExist = 0;
-		try {
-			amountExist = Integer.parseInt(amountExistStr);
-		} catch (Exception e) {
-		}
-
-		// 2. Gọi DAO lấy 4 phim TIẾP THEO
-		MovieDAO dao = new MovieDAO();
-		List<Movie> list = dao.getNextMovies(4, amountExist);
-
-		// 3. Đẩy list này sang file JSP Fragment
-		request.setAttribute("newList", list);
-
-		// 4. Trả về đoạn HTML (Không chuyển trang, chỉ trả về nội dung)
-		request.getRequestDispatcher("ajax-movie-list.jsp").forward(request, response);
-	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
+        
+        String type = request.getParameter("type");
+        int existed = Integer.parseInt(request.getParameter("existed"));
+        
+        MovieDAO dao = new MovieDAO();
+        
+        // SỬA SỐ 4 THÀNH SỐ 8 Ở ĐÂY
+        List<Movie> list = dao.getNextMovies(type, 8, existed); 
+        
+        request.setAttribute("newList", list);
+        request.getRequestDispatcher("ajax-movie-card.jsp").forward(request, response);
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
