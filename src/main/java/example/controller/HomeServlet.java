8e55e40f9d992a180446c13233f7efb84ba5d953
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-23 17:11:08 +0700

    them thuoc tinh lay 8 phim trong danh sach phim dang chieu hoac sap chieu

--- src/main/java/example/controller/HomeServlet.java
+++ src/main/java/example/controller/HomeServlet.java
@@ -19,12 +19,18 @@ public class HomeServlet extends HttpServlet {
 
 		MovieDAO dao = new MovieDAO();
 
+		List<Movie> now8ShowingList = dao.get8NowShowingMovies();
 		List<Movie> nowShowingList = dao.getNowShowingMovies();
 
-		List<Movie> comingSoonList = dao.getComingSoonMovies();
+		List<Movie> coming8SoonList = dao.get8ComingSoonMovies();
+		List<Movie> comingSoonList = dao.getComingSoonMovies();		
 
+		request.setAttribute("now8ShowingList", now8ShowingList);
 		request.setAttribute("nowShowingList", nowShowingList);
+
+		request.setAttribute("coming8SoonList", coming8SoonList);
 		request.setAttribute("comingSoonList", comingSoonList);
+
 
 		request.getRequestDispatcher("homepage.jsp").forward(request, response);
 	}
