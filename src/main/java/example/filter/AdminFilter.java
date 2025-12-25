da204dd9303a33e2dd579679fa73d030abc8f434
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-25 18:20:43 +0700

    them folder filter

---
+++ src/main/java/example/filter/AdminFilter.java
@@ -0,0 +1,61 @@
+package example.filter;
+
+import java.io.IOException;
+
+import javax.annotation.processing.Filer;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import example.model.system.User;
+
+/**
+ * Servlet implementation class AdminFilter
+ */
+@WebFilter("/admin/*")
+public class AdminFilter implements Filter {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AdminFilter() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+
+		System.out.println("AdminFilter đang chặn request tới: " + request.getRequestURI());
+		
+		HttpSession session = request.getSession();
+
+		User user = (User) session.getAttribute("user");
+
+		if (user == null) {
+			response.sendRedirect(request.getContextPath() + "/login");
+			return;
+		}
+
+		if (!user.getRole().equalsIgnoreCase("admin")) {
+			response.sendError(HttpServletResponse.SC_FORBIDDEN);
+			return;
+		}
+		chain.doFilter(request, response);
+
+
+	}
+
+}
