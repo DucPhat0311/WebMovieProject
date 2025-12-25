83f45fbc360c80efb6f386275fd78389b23ce006
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-25 23:14:58 +0700

    them method findByEmail thay cho login method vi hashedpassword

--- src/main/java/example/dao/impl/UserDAO.java
+++ src/main/java/example/dao/impl/UserDAO.java
@@ -98,4 +98,29 @@ public class UserDAO {
 		}
 		return null;
 	}
+	
+	public User findByEmail(String email) {
+	    User user = null;
+	    String sql = "SELECT * FROM user WHERE email = ?"; 
+
+	    try {
+	        Connection conn = new DBConnection().getConnection(); 
+	        PreparedStatement ps = conn.prepareStatement(sql);
+	        ps.setString(1, email);
+	        ResultSet rs = ps.executeQuery();
+
+	        if (rs.next()) {
+	            user = new User();
+	            user.setUserId(rs.getInt("user_id"));
+	            user.setEmail(rs.getString("email"));
+	            user.setPassword(rs.getString("password"));
+	            user.setRole(rs.getString("role"));
+	            user.setFullName(rs.getString("full_name"));
+	        }
+	        conn.close();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return user;
+	}
 }
